dc0c8332f5971005a13b30d6e31a1cfa5f61224b
Nik McKnight <60225340+NikMcKnight@users.noreply.github.com>
2020-10-06 12:39:52 -0500

    Add files via upload
    Added what I have for the board so far and a corrected main file

--- src/Board.java
+++ src/Board.java
@@ -1,5 +1,44 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
 
 public class Board {
+	Board(int numPieces){
+		for (int i = 0; i < numPieces; i++) {
+			whitePieces.add(0);
+			blackPieces.add(0);
+			}
+		this.readSpaces();
+		System.out.println(whitePieces);
+		}
+	
+	HashMap<Integer, List<Integer>> spaces = new HashMap<Integer, List<Integer>>();
+	List<Integer> whitePieces = new ArrayList<Integer>();
+	List<Integer> blackPieces = new ArrayList<Integer>();
 
+
+
+public void readSpaces() {
+	try (Scanner scanner = new Scanner(new File("src\\9MM Spaces.txt"))) {
+		int tempKey = -1;
+		List<Integer> tempVals = new ArrayList<Integer>();
+		Scanner nextLine;
+
+		
+		while (scanner.hasNext()) {
+			nextLine = new Scanner(scanner.nextLine());
+			tempKey = nextLine.nextInt();
+			while (nextLine.hasNextInt()) {
+				tempVals.add(nextLine.nextInt());
+			}
+			spaces.put(tempKey, tempVals);
+			System.out.print("Key: " + tempKey + "  Valid Moves: " + spaces.get(tempKey) + "\n");
+			tempVals.clear();
+		}
+	} catch (Exception e) {
+		System.err.println("something broke");
+	}
 }
-// 
+}
---
+++ src/Nine_Mens_morris.java
@@ -0,0 +1,9 @@
+
+public class Nine_Mens_morris {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Board test = new Board(9);
+	}
+
+}
