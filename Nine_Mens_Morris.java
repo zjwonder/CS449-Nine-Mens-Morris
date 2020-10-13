31904eaa0de1711436fb985b62f8ea545c949de5
zhalderwood <60233767+zhalderwood@users.noreply.github.com>
2020-10-12 22:16:02 -0500

    Update Nine_Mens_Morris.java

--- Nine_Mens_Morris.java
+++ Nine_Mens_Morris.java
@@ -3,8 +3,17 @@ public class Nine_Mens_Morris {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Board test = new Board(9);
-		Game_Logic gameTest = new Game_Logic();
+		String password = "hack me";
+		Scanner text_scan = new Scanner(System.in);
+		String test_passcode = text_scan.nextLine();
+		System.out.print("<" + test_passcode + ">"); // for testing
+		if (test_passcode == password) {
+			Test_Cases test_run = new Test_Cases();
+		}
+		else {
+			Board test = new Board(9);
+			Game_Logic gameTest = new Game_Logic();
+		}
 	}
 
 }
