f075cc766e42be5a2f4064da4be5cb8c774e7f63
BLGFQY <54372768+BLGFQY@users.noreply.github.com>
2020-11-10 13:08:39 -0600

    update to GUI

---
+++ src/GUI/GUI.java
@@ -0,0 +1,205 @@
+package GUI;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Scanner;
+
+import javafx.util.Pair;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class GUI extends Application implements EventHandler<ActionEvent> {
+
+	/****************variables*****************************************************/
+	// HashMap with Key == board location  and  Value == coordinate location
+	HashMap<Integer, Pair<Integer, Integer>> targetInfo = new HashMap<Integer, Pair<Integer, Integer>>(); 
+	// HashMap with Key == coordinate location  and  Value == board location
+	HashMap<Pair<Integer, Integer>, Integer> reverseInfo = new HashMap<Pair<Integer, Integer>, Integer>();
+	// list of target locations on board
+	ArrayList<Target> targets = new ArrayList<Target>();
+	// list of all white player pieces
+	ArrayList<Piece> whitePieces = new ArrayList<Piece>();
+	// list of all black player pieces
+	ArrayList<Piece> blackPieces = new ArrayList<Piece>();
+	
+	// display pane that all objects must go onto
+	Pane pane = new Pane();
+	
+	// display scene that defines all space within widow
+	Scene scene = new Scene(pane, 900, 700);
+		
+	
+	/****************constructors**************************************************/
+	//// base constructor ////
+	public GUI() {
+		
+	}
+	
+	//// extended constructors ////
+	
+	
+	
+	// do we need a destructor?????????????
+	
+	/****************getters*******************************************************/ 
+			
+			
+			
+	/****************setters*******************************************************/
+			
+			
+			
+	/****************misc functions************************************************/
+	@Override
+    public void start(Stage stage) {
+		
+		// Here we edit the window for aesthetics and readability
+    	stage.setTitle("Nine Men's Morris"); // sets title of window
+    	stage.setResizable(false);			 // disables ability to resize window
+    	final Text whiteText = new Text(15,10,"White Pieces");
+    	final Text blackText = new Text(815,10,"Black Pieces");				
+    	pane.getChildren().add(whiteText);
+    	pane.getChildren().add(blackText);
+    	
+    	// Here we create a background visual which displays the board
+    	ImageView mv = new ImageView("/img/nineMensMorris.png");
+    	mv.setX(150); mv.setY(50);
+    	pane.getChildren().addAll(mv);
+    	
+    	initPieces();
+    	readSpaces();
+    	//printReverseSpaces(reverseInfo);
+    	//printSpaces(targetInfo);
+    	initTargets();
+    	
+    	
+    	stage.setScene(scene);
+        stage.show(); 
+        
+        stage.setOnCloseRequest((event) -> {
+        	for (int i = 0; i < blackPieces.size(); i++) {
+            	System.out.println(i+1 + ": " + blackPieces.get(i).getPieceLoc() + " " + whitePieces.get(i).getPieceLoc());
+            }
+        });
+    }
+	
+	// function creates all player pieces
+	public void initPieces() {
+		// for loop creates 9 white player pieces, sets size and location, and adds them to list/pane
+		for (int i = 0; i < 9; i++) {
+			Piece tempWhite = new Piece("white");
+			tempWhite.makePiece();
+			whitePieces.add(tempWhite);
+			pane.getChildren().add(tempWhite.pieceImg);
+			
+			Piece tempBlack = new Piece("black");
+			tempBlack.makePiece();
+			blackPieces.add(tempBlack);
+			pane.getChildren().add(tempBlack.pieceImg);
+    	}
+	}
+
+	// function creates all target locations on board
+	public void initTargets() {
+		
+		// while loop gets target location information from hashmap and stores values in temp variables
+		// then creates targets, sets their location/size, and adds them to the pane/list
+		Iterator ite = targetInfo.entrySet().iterator();
+    	while (ite.hasNext()) {
+    		Map.Entry pair = (Map.Entry)ite.next();
+    		Integer boardLoc = (int) pair.getKey();
+    		Pair<Integer, Integer> coordinates = (Pair<Integer, Integer>) pair.getValue();
+    		ite.remove();
+    		
+    		Target target = new Target(coordinates, boardLoc);
+    		target.makeTarget();
+    		targets.add(target);
+    		pane.getChildren().add(target.targetImg);
+    	}
+	}
+	
+	public int findLoc(Pair<Integer, Integer> coords) {
+		System.out.println("coords = " + coords);
+		System.out.println("reverseInfo.containsKey(coords) = " + reverseInfo.containsKey(coords) + "\n");
+		if (reverseInfo.containsKey(coords)){
+			return reverseInfo.get(coords);
+		}
+		else
+			return -2;
+	}
+
+	
+	
+	// function takes in target location info from spaces.txt and stores them in HashMap targetInfo
+	public void readSpaces() {
+		try {
+			// scanner for each line
+	   		Scanner scanner = new Scanner(new File("src\\spaces.txt"));
+	   		int tempKey, tempX, tempY;
+	   		// scanner for parsing each line individually
+	   		Scanner nextLine;
+		    		
+	   		while(scanner.hasNext()) {
+	   			nextLine = new Scanner(scanner.nextLine());
+	   			tempKey = nextLine.nextInt();
+	   			if (tempKey == -1)
+	   				break;
+	   			tempX = nextLine.nextInt();
+	   			tempY = nextLine.nextInt();
+	   			
+	   			Pair<Integer, Integer> tempVals = new Pair<Integer, Integer>(tempX, tempY);
+	   			targetInfo.put(tempKey, tempVals);
+	   			reverseInfo.put(tempVals, tempKey);
+	   			nextLine.close();
+	   		}
+	   	scanner.close();	
+	   	} catch(Exception e) {
+	   		System.err.println("reading error");
+	   	}
+	}
+			
+	// function prints out spaces stored in HashMap targets to verify everything is being read in correctly
+	public static void printSpaces(HashMap targetInfo) {
+	   	Iterator ite = targetInfo.entrySet().iterator();
+	   	while (ite.hasNext()) {
+	   		Map.Entry pair = (Map.Entry)ite.next();
+	   		Pair<Integer, Integer> value = (Pair<Integer, Integer>) pair.getValue();
+	   		System.out.println("Key: " + pair.getKey() + "  X val: " + value.getKey() + "  Y val: " + value.getValue());
+	   		ite.remove();
+	   	}
+	}
+		    
+	// function prints out spaces stored in HashMap targets to verify everything is being read in correctly
+	public static void printReverseSpaces(HashMap reverseInfo) {
+	   	Iterator ite = reverseInfo.entrySet().iterator();
+	   	while (ite.hasNext()) {
+	   		Map.Entry pair = (Map.Entry)ite.next();
+	   		Pair<Integer, Integer> value = (Pair<Integer, Integer>) pair.getKey();
+	   		System.out.println("Key X val: " + value.getKey() + "   Key Y val: " + value.getValue() + "   Value: " + pair.getValue());
+	   		ite.remove();
+	   	}
+	}
+
+	// function for setting value that determines if the user can interact with white pieces
+    // function that will makes white pieces move-able or not, depending on current state
+
+    
+    public static void main(String[] args) {
+        launch();
+    }
+
+	@Override
+	public void handle(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+}
---
+++ src/GUI/Piece.java
@@ -0,0 +1,115 @@
+package GUI;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TransferMode;
+import javafx.util.Pair;
+
+public class Piece extends GUI implements EventHandler<ActionEvent> {
+
+	// variables
+	ImageView pieceImg; // stores the actual image and subject of drag & drop properties
+	Pair<Integer, Integer> coordinates; // stores the coordinate location of the piece
+	int pieceLoc;		// stores the current location of the piece (ex. 11, 14, 17)
+	String pieceClr;	// stores the color of the piece
+	
+	/*************constructors*****************************************************/
+	//// base constructor ////
+	public Piece() {}
+	
+	////extended constructors ////
+	// constructor to differentiate between colors
+	public Piece(String color) {
+		pieceLoc = -1;
+		if (color == "white") {
+			pieceImg = new ImageView("/img/white_transparent.png");
+			pieceImg.setX(50); coordinates = new Pair<Integer, Integer>(50, 50);
+		}
+		else if (color == "black") {
+			pieceImg = new ImageView("/img/black_transparent.png");
+			 pieceImg.setX(825); coordinates = new Pair<Integer, Integer>(825, 50);
+		}
+		else {
+			System.out.print("Error in Piece constructor");
+		}
+		pieceClr = color;
+		pieceImg.setFitHeight(35); pieceImg.setFitWidth(35);
+		pieceImg.setY(50);
+	}
+	
+	// do we need a destructor?????????????
+	
+	/****************getters*******************************************************/ 
+	
+	// returns pieces current locaton
+	public int getPieceLoc() {
+		return pieceLoc;
+	}
+	
+	// returns piece color
+	public String getPieceClr() {
+		return pieceClr;
+	}
+	
+	public Pair<Integer, Integer> getCoords() {
+		return coordinates;
+	}
+	
+	/****************setters*******************************************************/
+	
+	// sets the location of piece
+	public void setPieceLoc(int location) {
+		pieceLoc = location;
+	}
+	
+	// sets value of coordinates variable
+	public void setBoardLoc() {
+		
+	}
+	
+	/*********************misc functions*******************************************/
+	
+	// function gives drag & drop properties to pieceImg variable
+	public void makePiece() {
+	    pieceImg.setOnDragDetected(new EventHandler<MouseEvent>() {
+	        public void handle(MouseEvent event) {
+	            /* drag was detected, start a drag-and-drop gesture*/
+	            /* allow any transfer mode */
+	            Dragboard db = pieceImg.startDragAndDrop(TransferMode.ANY);
+	            
+	            /* Put a string on a dragboard */
+	            ClipboardContent content = new ClipboardContent();
+	            content.putImage(pieceImg.getImage());
+	            db.setContent(content);
+	            event.consume();
+	        }
+	    });
+
+	    
+	    pieceImg.setOnDragDone(new EventHandler<DragEvent>() {
+	        public void handle(DragEvent event) {
+	            /* the drag and drop gesture ended */
+	            /* if the data was successfully moved, clear it */
+	            if (event.getTransferMode() == TransferMode.MOVE) {
+	                pieceImg.setImage(null);
+	            }
+	            event.consume();
+
+	        }
+	    });
+	}
+	
+	
+	@Override
+	public void handle(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+}
+
+
