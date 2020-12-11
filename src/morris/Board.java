598976dff139518a14c541228f064b7fa0fd0476
zhalderwood <60233767+zhalderwood@users.noreply.github.com>
2020-12-10 20:46:41 -0600

    Code check with minor fixes

--- src/morris/Board.java
+++ src/morris/Board.java
@@ -13,31 +13,32 @@ import java.util.Set;
 
 public class Board {
 	
-	//Keeps track of what spaces are connected to a given space
+	// Keeps track of what spaces are connected to a given space
 	private HashMap<Integer, List<Integer>> connections = new HashMap<Integer, List<Integer>>(); 	
 	
 	boolean flyingAllowed = true;
 	
-	//list of all spaces on the board
+	// List of all spaces on the board
 	public List<Integer> spaces = new ArrayList<Integer>();
 	
-	// These are arraylists (dynamic arrays) that keep track of player pieces. 
+	// These are lists that keep track of player pieces. 
 	// All pieces start with value 0, are updated to the value of the spot where they're placed, and are deleted when a piece is removed from the board.
 	public List<Integer> whitePieces = new ArrayList<Integer>(); 
 	public List<Integer> blackPieces = new ArrayList<Integer>(); 
 	
-	//list of all valid mills
+	// List of all valid mills
 	private List<int[]> mills = new ArrayList<int[]>(); 
 	
-	// This is a constructor that should make it easier to implement different versions of the game later on (e.g. 3/6/12 Men's Morris).
+	
+	// Constructor for different versions of the game
 	public Board(int numPieces){ 
 		readSpaces(numPieces);
 		for (int i = 0; i < numPieces; i++) {
 			whitePieces.add(0);
 			blackPieces.add(0);
 		}
-		//for (int space : spaces) System.out.println(space);
 	}
+	
 	
 	// Resets board to initial state
 	public void reset(int numPieces) {
@@ -46,34 +47,39 @@ public class Board {
 		for (int i = 0; i < numPieces; i++) {
 			whitePieces.add(0);
 			blackPieces.add(0);
-			
 		}
 	}
 	
-	// getter for finding the number of pieces a player has
+	
+	// Getter for finding the number of pieces a player has
 	public int getNumPieces(boolean isWhiteTurn){ 
 		if (isWhiteTurn == true) return whitePieces.size();
 		else return blackPieces.size();
 	}
 	
+	
 	public List<Integer> getSpaces() {
 		return spaces;
 	}
 	
+	
 	public List<int[]> getMills() {
 		return mills;
 	}
+	
 	
 	//Getter for all connections
 	public HashMap<Integer, List<Integer>> getConnections() { 
 		return connections;
 	}
 	
+	
 	//Getter for the pieces a particular player owns
 	public List<Integer> getPieces(boolean isWhiteTurn) {
 		if (isWhiteTurn == true) return whitePieces;
 		else return blackPieces;
 	}
+	
 	
 	// returns location of the piece at given index
 	public int getLocation(boolean isWhiteTurn, int index) { 
@@ -99,12 +105,14 @@ public class Board {
 		return false;	
 	}
 
+	
 	//Returns true if given space is empty
 	public boolean spaceAvailable(int space) {
 		if (whitePieces.contains(space) == true || blackPieces.contains(space) == true) return false;
 		return true;
 	}
 		
+	
 	//Moves piece if move is valid and updates back end code
 	public boolean movePiece(boolean isWhiteTurn, int oldSpace, int newSpace) { // relocate piece
 		List<Integer> playerPieces = new ArrayList<Integer>();
@@ -112,12 +120,12 @@ public class Board {
 		else playerPieces = blackPieces;
 		if (!spaceAvailable(newSpace) || (!connections.get(oldSpace).contains(newSpace) && playerPieces.size() > 3)) { 
 			//make sure neither player has the desired space and new space is a valid movement from old space
-			//System.out.println("Not a valid move.");
 			return false;
 		}
 		playerPieces.set(playerPieces.indexOf(oldSpace), newSpace);
 		return true;
 	}
+	
 	
 	//Removes piece from the appropriate arraylist
 	public boolean removePiece(boolean isWhiteTurn, int piece) {
@@ -132,6 +140,7 @@ public class Board {
         return false;
     }
 		
+	
 	//Checks if a given piece formed a mill
 	public boolean checkMill(boolean isWhiteTurn, Integer newSpace) {
         List<Integer> tempPieces = new ArrayList<Integer>();
@@ -155,6 +164,7 @@ public class Board {
         return false;
     }
 	
+	
 	//Checks if all of a player's piece are in mills. This is used to determine if a piece in a mill can be removed by an opponent.
 	public boolean allPiecesInMills(boolean isWhiteTurn) {
 		List<Integer> opponentPieces = new ArrayList<Integer>();
@@ -167,7 +177,8 @@ public class Board {
         return true;
 	}
 		
-	//Checks what phase a given player is in. Also functions as a win check.
+	
+	// Checks what phase a given player is in. Also functions as a win check.
 	public int checkPhase(boolean isWhiteTurn) {
 		int piecesRemaining;
 		List<Integer> playerPieces = new ArrayList<Integer>();
@@ -187,7 +198,8 @@ public class Board {
 		else return 2;
 	}
 	
-	//Gets all valid moves for a selected piece
+	
+	// Gets all valid moves for a selected piece
 	public Set<Integer> getPossibleMoves(int moveFromID, int phase) {
 		Set<Integer> possibleMoves = new HashSet<Integer>();
 		if (phase == 2 || (phase == 3 && !flyingAllowed)) {
@@ -203,7 +215,8 @@ public class Board {
 		return possibleMoves;
 	}
 	
-	//Self explanatory
+	
+	// Checks victory conditions
 	public boolean checkForWin(boolean isWhiteTurn, int phase) {
 		List <Integer> playerPieces = new ArrayList<Integer>();
 		if (isWhiteTurn == true) playerPieces = blackPieces; 
@@ -214,7 +227,7 @@ public class Board {
 		return false;
 	}
 	
-	//returns true if the player has any valid moves
+	// Returns true if the player has any valid moves
 	public boolean anyPossibleMoves(boolean isWhiteTurn, int phase) {
 		List <Integer> playerPieces = new ArrayList<Integer>();
 		if (isWhiteTurn == true) playerPieces = blackPieces; 
@@ -226,7 +239,7 @@ public class Board {
 		return false;
 	}
 	
-	//Used in checkMill
+	// Basic search method // Used in checkMill
 	public boolean search(int[] array, int key) {
 		for(int i = 0; i < array.length; i++) {
 			if (array[i] == key) {
@@ -236,7 +249,7 @@ public class Board {
 		return false;
 	}
 	
-	// Reads a text file with all the valid spaces and their valid connections. This should also make different versions easier to implement.
+	// Reads a text file with all the valid spaces and their valid connections. // reads different file based on chosen game type
 	public boolean readSpaces(int numPieces) {
 		File boardData;
 		flyingAllowed = true;
@@ -248,9 +261,7 @@ public class Board {
 		else boardData = new File("src\\game data\\9MM board.txt");
 		try (Scanner scanner = new Scanner(boardData)) {
 			int tempKey;
-			
 			Scanner nextLine;
-	
 			while (scanner.hasNext()) { // loops until end of file
 				List<Integer> tempVals = new ArrayList<Integer>();
 				nextLine = new Scanner(scanner.nextLine()); // reads next line of file
@@ -267,7 +278,6 @@ public class Board {
 				nextLine.close();
 				
 			}
-			
 			while (scanner.hasNext()) { // loops until end of file
 				nextLine = new Scanner(scanner.nextLine());
 				int[] tempMill = new int[3];
@@ -276,7 +286,6 @@ public class Board {
 				}
 				mills.add(tempMill);
 				nextLine.close();
-			
 			}
 		scanner.close();
 		return true;
@@ -291,12 +300,6 @@ public class Board {
 	
 	// randomly generates a location to place a piece
 	public int AIplace() {
-//		for (int i = 0; i < spaces.size(); i++) {
-//			if (checkMill(false, spaces.get(i))) {
-//				System.out.println("i = " + i + "  location = " + spaces.get(i));
-//				return i;
-//			}
-//		}
 		Random rand = new Random();
 		int index = rand.nextInt(1000) % spaces.size();
 		while (!spaceAvailable(spaces.get(index))) {
@@ -304,6 +307,7 @@ public class Board {
 		}
 		return index;
 	}
+	
 	
 	// randomly chooses a white piece to remove when the AI creates a mill
 	public int AIchoose() {
@@ -317,7 +321,8 @@ public class Board {
 		System.out.println(index + " position: " + spaces.get(index));
 		return index;
 	}
-		
+	
+	
 	// method random picks a piece to move, returns its index
 	public int AIpiece(int blackPhase) {
 		Random rand = new Random();
@@ -333,11 +338,11 @@ public class Board {
 		return index;
 	}
 	
+	
 	// method randomly chooses which location to move to, based on piece chosen in AIpiece
 	public int AImove(int pieceIndex, int blackPhase) {
 		Random rand = new Random();
 		Set<Integer> moves = getPossibleMoves(spaces.get(pieceIndex), blackPhase);
-//		System.out.println("pieceIndex = " + pieceIndex + "  moves = " + moves);
 		int index = rand.nextInt(1000) % moves.size();		
 		
 		int counter = 0;
