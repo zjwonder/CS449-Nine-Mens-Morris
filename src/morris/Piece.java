7a47a308b8514273ad83655f5519b6830b02f818
zhalderwood <60233767+zhalderwood@users.noreply.github.com>
2020-12-10 20:47:08 -0600

    Code check with minor fixes

--- src/morris/Piece.java
+++ src/morris/Piece.java
@@ -16,23 +16,10 @@ public class Piece extends Button{
     private Image activePiece = new Image("file:src\\img\\Orb.png", 50, 50, false, false);
     private Boolean isActiveImageOn = false;
     
-    
-	public Piece(int ID, int xCoord, int yCoord) {
-		pieceID = ID;
-		setStyle("-fx-border-color: transparent;"
-        		+ "-fx-border-width: 0;"
-        		+ "-fx-background-radius: 0;"
-        		+ "-fx-background-color: transparent;"
-        		+ "-fx-font-family:\"Segoe UI\", Helvetica, Arial, sans-serif;"
-        		+ "-fx-font-size: 1em; /* 12 */"
-        		+ "-fx-text-fill: #828282;");
-        setPrefSize(50, 50);
-        setLayoutX(xCoord);
-        setLayoutY(yCoord);
-		player = ePlayer.none;
-	}
-	
+
+	// Default constructor
 	public Piece() {
+		System.out.println("This piece has no home!");
 		setVisible(false);
 		pieceID = 0;
 		setStyle("-fx-border-color: transparent;"
@@ -49,48 +36,63 @@ public class Piece extends Button{
 	}
 
 	
+	// Sets button coordinates on screen
 	public void setCoords(int x, int y) {
 		 setLayoutX(x);
 	     setLayoutY(y);
 	}
 	
 	
+	// Sets piece ID
 	public void setID(int i) {pieceID = i;}
+	
+	
+	// Gets piece ID
 	public int getID() {return pieceID;}
 	
+	
+	// Sets piece owner
 	public void setOwner(boolean isWhiteTurn) {
 		if (isWhiteTurn == true) player = ePlayer.white;
 		else player = ePlayer.black;
 	}
 	
+	
+	// Removes piece owner
 	public void clearPlayer() {
 		player = ePlayer.none;
 		setGraphic(null);
 		isActiveImageOn = false;
 	}
 	
+	
+	// Checks if piece has an owner
 	public boolean isEmpty() {
 		if (player == ePlayer.none) return true;
 		return false;
 	}
 	
+	
+	// Checks if piece is active
 	public boolean isActiveImageOn() {
 		if (isActiveImageOn == true) return true;
 		return false;
 	}
 	
+	
+	// Activates piece and sets image to glowing space
 	public void setActiveImage() {
 		setGraphic(new ImageView(activePiece));
 		isActiveImageOn = true;		
 	}
 	
+	
+	// Sets piece image to white or black piece
 	public void setPlayer(boolean isWhiteTurn) {
 		if (isWhiteTurn == true) setGraphic(new ImageView(whitePiece));
 		if (isWhiteTurn == false) setGraphic(new ImageView(blackPiece));
 		isActiveImageOn = false;
-	}
-	
-	
+	}	
 }
 
 
